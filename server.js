6527556ef3c2332aaad6d581d6a2927c7abb8187
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-06-21 22:44:33 +0300

    Set up websockets on the server side, port: 8081.

--- server.js
+++ server.js
@@ -8,3 +8,29 @@ const staticServer = new static.Server('.');
 const server = http.createServer((req, res) => {
   staticServer.serve(req, res);
 }).listen(port);
+
+
+
+const WebSocket = require('ws');
+
+const wsPort = 8081;
+const wss = new WebSocket.Server({port: wsPort});
+
+wss.on('connection', (ws) => {
+
+  //let id = Math.random();
+  console.log('Connection opened.');
+
+  ws.on('message', (msg) => {
+    console.log(`received: ${msg}`);
+
+    wss.clients.forEach((client) => {
+      if (client.readyState !== WebSocket.OPEN) return;
+      client.send(msg);
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Connection closed.');
+  });
+});
